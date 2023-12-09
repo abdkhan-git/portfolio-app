3f3d3d0b29ac9fb4dcc1fce53ec57a8cd8ada685
Abdullah Khan <abdkhan033@gmail.com>
2023-12-09 02:10:37 -0500

    Deployment Update

--- src/theme/icons.ts
+++ src/theme/icons.ts
@@ -1,4 +1,4 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { IconDefinition, library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import {
   faHome,
